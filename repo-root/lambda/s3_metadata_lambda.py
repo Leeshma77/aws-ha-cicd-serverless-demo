c7bcf7eee91f1dfcb95dcd4d3168ec9e53e99c44
Leeshma77 <leeshma2025@gmail.com>
2026-03-02 12:29:22 +0530

    Add S3 metadata processing Lambda function

---
+++ repo-root/lambda/s3_metadata_lambda.py
@@ -0,0 +1,25 @@
+import os
+import boto3
+from urllib.parse import unquote_plus
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ.get('DDB_TABLE', 'FileMetadata'))
+s3 = boto3.client('s3')
+
+def handler(event, context):
+    results = []
+    for rec in event.get('Records', []):
+        bucket = rec['s3']['bucket']['name']
+        key    = unquote_plus(rec['s3']['object']['key'])
+        head   = s3.head_object(Bucket=bucket, Key=key)
+        item = {
+            'filename': key,
+            'bucket': bucket,
+            'size': head['ContentLength'],
+            'contentType': head.get('ContentType', 'unknown'),
+            'etag': head.get('ETag', '').strip('"'),
+            'status': 'processed'
+        }
+        table.put_item(Item=item)
+        results.append(item)
+    return {'ok': True, 'count': len(results)}
